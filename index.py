93c381a17613dd317d79e679db05f541fdc29838
emin_bzdg_147 <64196760+1-4-7@users.noreply.github.com>
2022-10-19 11:53:52 +0300

    love

---
+++ index.py
@@ -0,0 +1,16 @@
+import turtle
+
+turtle.bgcolor("black")
+turtle.color("red")
+turtle.begin_fill()
+turtle.pensize(3)
+turtle.speed(5)
+turtle.left(50)
+turtle.forward(133)
+turtle.circle(50,200)
+turtle.right(140)
+turtle.circle(50,200)
+turtle.forward(133)
+turtle.end_fill()
+turtle.hideturtle()
+turtle.mainloop()
